56c6ad3130dfd51da75a5407f408e02a5e05f5e7
Adrian <adrian.grochowski17@gmail.com>
2025-07-18 20:03:59 +0200

    Create placeholder page (under construction) for dashboard route

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,3 +1,17 @@
+import styles from './DashboardPage.module.css';
+
 export default function DashboardPage() {
-	return <div>DashboardPage</div>;
+	return (
+		<div className={styles.dashboardWrapper}>
+			<h2 className={styles.textHeding}>This page is under construction</h2>
+			<p className={styles.textSmall}>
+				We're working on it and will have it ready soon. Stay tuned!
+			</p>
+			<div className={styles.imageContainer}>
+				<div className={styles.gradientImg}></div>
+
+				<img src='/assets/under-construction.jpg' alt='' />
+			</div>
+		</div>
+	);
 }
